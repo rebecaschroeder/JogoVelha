55df91cd24af42d9d7f015f40fbff45883493aba
rebecaschroeder <rebeca.schroeder@gmail.com>
2018-10-15 12:46:30 -0300

    Initial commit

---
+++ src/Computer.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrador
+ */
+public class Computer extends Player{
+    
+    public Computer(int player){
+        super(player);
+        System.out.println("Player 'Computer' created");
+    }
+    
+    @Override
+    public void play(Board board){
+        
+    }
+    
+    @Override
+    public void Try(Board board){
+        
+    }
+}
